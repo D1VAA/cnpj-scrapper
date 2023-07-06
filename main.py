b698074838e164dce3f52c3caa5caf810a859b9a
HyBun <davipare203@gmail.com>
2023-07-06 18:12:56 -0300

    feat: add argparser (temporary)

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
 import pandas as pd
 from packages.dfmanager.DataFrameManager import DataframeManager
-from packages.scrapper import CNPJScrapper
+from packages.scrapper import Scrapper
 import asyncio
 from argparse import ArgumentParser
 
+
+parser = ArgumentParser(description='Executa uma busca em uma lista de CNPJs e retorna as informações das organizações')
+parser.add_argument('-s', '--site', help='Seleciona o site que será feito a busca. Opções: cnpj.biz ou speedio')
+args = parser.parse_args()
+site = args.site
+
+assert site is not None, 'Run: python ./main.py -h'
 
 def get_cnaes():
     filterlist = {'Atividade': ['Comércio', 'Indústria']}
@@ -18,31 +25,30 @@ def get_cnaes():
 
     # Removing useless columns
     dataframe = dataframe.drop(columns=['Anexo', 'Tributação', 'FatorR', 'Alíq.(%)'], axis=1)
-    return dataframe['Código']
+    return dataframe
 
 
 def get_cnpjs(size=1):
+    """
+    :param size: value * 1000 (lines)
+    """
     filterlist = {'uf': ['SP', 'SC', 'RS']}
     data = DataframeManager('./sheets/empresas.csv', filters=filterlist)
     g_dataframe = data.get_dataframe(size)
 
     # Select cnpj column
-    dataframe = g_dataframe
-    return dataframe['cnpj']
+    dataframe = g_dataframe['cnpj']
+    return dataframe
 
 
-f_cnaes = get_cnaes()
-cnpjs = get_cnpjs(1)
+#cnaes = get_cnaes()
+cnpjs = get_cnpjs()
 
-parser = ArgumentParser(description='Executa uma busca em uma lista de CNPJs e retorna as informações das organizações')
-parser.add_argument('-s', '--site', help='Seleciona o site que será feito a busca. Opções: cnpj.biz ou speedio')
-args = parser.parse_args()
-site = args.site
-assert site is not None, 'Run: python ./main.py -h'
+consulta = Scrapper(cnpjs, site)
+resultado = consulta.run(show_results=True)
+resultado['CNAES'] = resultado['CNAES'].str.split(',')
+resultado = resultado.explode('CNAES')
+resultado.to_excel('Result.xlsx')
 
-consulta = CNPJScrapper(cnpjs, site)
-resultado = asyncio.run(consulta.execute(verbose=True, show_results=True))
-filters = {'CNAES': f_cnaes}
-exp = DataframeManager(data=resultado)
-exp.apply_filters(filters)
-print(exp)
+#filters = {'CNAES': cnaes}
+#exp.apply_filters(filters)
